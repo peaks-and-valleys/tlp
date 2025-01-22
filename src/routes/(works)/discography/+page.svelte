81eb7f3a01e346efd5f22312bdc49d2ac167d843
peaksvndvalleys <39687819+peaksvndvalleys@users.noreply.github.com>
2025-01-22 19:25:56 +0900

    feat(routes): restructure the layout of the discography and past shows pages to improve readability and organization style(routes): update the title and description for better SEO and clarity fix(routes): correct the capitalization of "Past Shows" for consistency across the site

--- src/routes/(works)/discography/+page.svelte
+++ src/routes/(works)/discography/+page.svelte
@@ -8,233 +8,239 @@
 	<title>Discography - FATAL WOVND</title>
 </svelte:head>
 
-<h1>Discography</h1>
-<h2>Solo Works</h2>
-<h3>Albums / EPs</h3>
-<ul>
-	<li>
-		<a href="https://ffm.to/pvnkrxck" target="_blank">Pvnk Rxck</a>
-		(2023-07-01, FATAL WOVND)
-	</li>
-	<li>
-		<a href="https://ffm.to/deadandfvcked" target="_blank">DEAD AND FVCKED!</a>
-		(2022-08-07, FATAL WOVND)
-	</li>
-	<li>
-		<a href="https://ffm.to/goingwrong" target="_blank">Going Wrong</a>
-		(2022-01-15, FATAL WOVND)
-	</li>
-	<li>
-		<a href="https://ffm.to/notallthesame" target="_blank">Not All The Same!</a>
-		(2021-01-10, Day of the Droids)
-	</li>
-	<li>
-		<a href="https://ffm.to/peakystyle" target="_blank">Peakystyle</a>
-		(2020-02-08, Sadistik Noize Asylum)
-	</li>
-	<li>
-		<a
-			href="https://viralconspiracyrecords.bandcamp.com/album/tohlpeaks-all-fools-shall-bleed"
-			target="_blank">All Fools Shall Bleed</a
-		>
-		(2016-10-15, Viral Conspiracy Records)
-	</li>
-</ul>
-<h3>Singles</h3>
-<ul>
-    <li>
-		<a href="https://ffm.to/n0ten0ugh" target="_blank">n0t en0ugh</a>
-		(2022-10-31, FATAL WOVND)
-	</li>
-	<li>
-		<a href="https://terminalexplosion.bandcamp.com/track/inside-my-head" target="_blank"
-			>Inside My Head</a
-		>
-		(2019-05-30, Terminal Explosion)
-	</li>
-	<li>
-		<a href="https://antistatic.bandcamp.com/album/tohlpeaks-peaky-hate-machine" target="_blank"
-			>Peaky Hate Machine</a
-		>
-		(2018-11-26, Antistatic)
-	</li>
-	<li>
-		<a href="https://sadistiknoizeasylum.bandcamp.com/album/devils-in-my-blood" target="_blank"
-			>Devils In My Blood</a
-		>
-		(2017-11-13, Sadistik Noize Asylum)
-	</li>
-</ul>
-<h2>Appearances / Collaborations</h2>
-<h3>2024</h3>
-<ul>
-	<li>
-		<a href="https://sound.jp/fujimi/fir167.html" target="_blank"
-			>IMMORTAL HARDCORE!!!! VOL.7 -Bloody Deathmatch-</a
-		>
-		(Comp., 2024-04-28, Fujimi Industry Records)
-	</li>
-</ul>
-<h3>2023</h3>
-<ul>
-	<li>
-		<a href="https://zisatsu.bandcamp.com/album/uicide-elect-vol-2" target="_blank"
-			>$UICIDE​$​ELECT Vol​.​2</a
-		>
-		(Comp., 2023-08-04, $uiciderecord$)
-	</li>
-	<li>
-		<a href="https://zisatsu.bandcamp.com/album/uicide-elect-vol-1" target="_blank"
-			>$UICIDE​$​ELECT Vol​.​1</a
-		>
-		(Comp., 2023-01-27, $uiciderecord$)
-	</li>
-</ul>
-<h3>2022</h3>
-<ul>
-	<li>
-		<a href="https://lossokarecords.bandcamp.com/album/genocide-afterparty" target="_blank">
-			Genocide Afterparty</a
-		>
-		(Comp., 2022-06-16, Lossoka Records)
-	</li>
-	<li>
-		<a href="https://sound.jp/fujimi/fir125.html" target="_blank"
-			>IMMORTAL HARDCORE!!!! VOL.6 -Black Out-</a
-		>
-		(Comp., 2022-04-24, Fujimi Industry Records)
-	</li>
-	<li>
-		<a href="https://zisatsu.bandcamp.com/album/depressive-breakcore-vol-4" target="_blank"
-			>Depressive Breakcore vol.4</a
-		>
-		(Comp., 2022-04-02, $uiciderecord$)
-	</li>
-</ul>
-<h3>2021</h3>
-<ul>
-	<li>
-		<a href="https://zisatsu.bandcamp.com/album/hers-movie" target="_blank"
-			>彼女の映画 ~Hers Movie~</a
-		>
-		(Comp., 2021-08-06, $uiciderecord$)
-	</li>
+<main>
+	<article>
+		<h1>Discography</h1>
+		<h2>Solo Works</h2>
+		<h3>Albums / EPs</h3>
+		<ul>
+			<li>
+				<a href="https://ffm.to/pvnkrxck" target="_blank">Pvnk Rxck</a>
+				(2023-07-01, FATAL WOVND)
+			</li>
+			<li>
+				<a href="https://ffm.to/deadandfvcked" target="_blank">DEAD AND FVCKED!</a>
+				(2022-08-07, FATAL WOVND)
+			</li>
+			<li>
+				<a href="https://ffm.to/goingwrong" target="_blank">Going Wrong</a>
+				(2022-01-15, FATAL WOVND)
+			</li>
+			<li>
+				<a href="https://ffm.to/notallthesame" target="_blank">Not All The Same!</a>
+				(2021-01-10, Day of the Droids)
+			</li>
+			<li>
+				<a href="https://ffm.to/peakystyle" target="_blank">Peakystyle</a>
+				(2020-02-08, Sadistik Noize Asylum)
+			</li>
+			<li>
+				<a
+					href="https://viralconspiracyrecords.bandcamp.com/album/tohlpeaks-all-fools-shall-bleed"
+					target="_blank">All Fools Shall Bleed</a
+				>
+				(2016-10-15, Viral Conspiracy Records)
+			</li>
+		</ul>
+		<h3>Singles</h3>
+		<ul>
+			<li>
+				<a href="https://ffm.to/n0ten0ugh" target="_blank">n0t en0ugh</a>
+				(2022-10-31, FATAL WOVND)
+			</li>
+			<li>
+				<a href="https://terminalexplosion.bandcamp.com/track/inside-my-head" target="_blank"
+					>Inside My Head</a
+				>
+				(2019-05-30, Terminal Explosion)
+			</li>
+			<li>
+				<a href="https://antistatic.bandcamp.com/album/tohlpeaks-peaky-hate-machine" target="_blank"
+					>Peaky Hate Machine</a
+				>
+				(2018-11-26, Antistatic)
+			</li>
+			<li>
+				<a href="https://sadistiknoizeasylum.bandcamp.com/album/devils-in-my-blood" target="_blank"
+					>Devils In My Blood</a
+				>
+				(2017-11-13, Sadistik Noize Asylum)
+			</li>
+		</ul>
+		<h2>Appearances / Collaborations</h2>
+		<h3>2024</h3>
+		<ul>
+			<li>
+				<a href="https://sound.jp/fujimi/fir167.html" target="_blank"
+					>IMMORTAL HARDCORE!!!! VOL.7 -Bloody Deathmatch-</a
+				>
+				(Comp., 2024-04-28, Fujimi Industry Records)
+			</li>
+		</ul>
+		<h3>2023</h3>
+		<ul>
+			<li>
+				<a href="https://zisatsu.bandcamp.com/album/uicide-elect-vol-2" target="_blank"
+					>$UICIDE​$​ELECT Vol​.​2</a
+				>
+				(Comp., 2023-08-04, $uiciderecord$)
+			</li>
+			<li>
+				<a href="https://zisatsu.bandcamp.com/album/uicide-elect-vol-1" target="_blank"
+					>$UICIDE​$​ELECT Vol​.​1</a
+				>
+				(Comp., 2023-01-27, $uiciderecord$)
+			</li>
+		</ul>
+		<h3>2022</h3>
+		<ul>
+			<li>
+				<a href="https://lossokarecords.bandcamp.com/album/genocide-afterparty" target="_blank">
+					Genocide Afterparty</a
+				>
+				(Comp., 2022-06-16, Lossoka Records)
+			</li>
+			<li>
+				<a href="https://sound.jp/fujimi/fir125.html" target="_blank"
+					>IMMORTAL HARDCORE!!!! VOL.6 -Black Out-</a
+				>
+				(Comp., 2022-04-24, Fujimi Industry Records)
+			</li>
+			<li>
+				<a href="https://zisatsu.bandcamp.com/album/depressive-breakcore-vol-4" target="_blank"
+					>Depressive Breakcore vol.4</a
+				>
+				(Comp., 2022-04-02, $uiciderecord$)
+			</li>
+		</ul>
+		<h3>2021</h3>
+		<ul>
+			<li>
+				<a href="https://zisatsu.bandcamp.com/album/hers-movie" target="_blank"
+					>彼女の映画 ~Hers Movie~</a
+				>
+				(Comp., 2021-08-06, $uiciderecord$)
+			</li>
 
-	<li>
-		<a href="https://sound.jp/fujimi/fir110.html" target="_blank">Homicide Vol.3</a>
-		(Comp., 2021-08-05, Fujimi Industry Records)
-	</li>
-	<li>
-		<a href="https://okuyukashiki.bandcamp.com/album/3" target="_blank">ばり好いコンピ3</a>
-		(Comp., 2021-04-25, 架空インターネットラジオ「ばり好いと〜よ！」)
-	</li>
-</ul>
-<h3>2020</h3>
-<ul>
-	<li>
-		<a href="https://zisatsu.bandcamp.com/album/her-picture" target="_blank"
-			>彼女の写真 -Her picture-</a
-		>
-		(Comp., 2020-08-10, $uiciderecord$)
-	</li>
-	<li>
-		<a href="http://sound.jp/fujimi/fir088.html" target="_blank">Homicide Vol.2</a>
-		(Comp., 2020-06-19, Fujimi Industry Records)
-	</li>
-	<li>
-		<a href="https://okuyukashiki.bandcamp.com/" target="_blank">ばり好いコンピ</a>
-		(Comp., 2020-03-01, 架空インターネットラジオ「ばり好いと〜よ！」)
-	</li>
-</ul>
-<h3>2019</h3>
-<ul>
-	<li>
-		<a href="https://sna003.tumblr.com/" target="_blank"
-			>Sadistik Noize Addiction Vol.2 -Violence of Hell-</a
-		>
-		(Comp., 2019-10-27, Sadistik Noize Asylum)
-	</li>
-	<li>
-		<a href="http://sound.jp/fujimi/fir078.html" target="_blank">Coakira - Sickle</a>
-		(Remix, 2019-10-27, Fujimi Industry Records)
-	</li>
-	<li>
-		<a href="http://sound.jp/fujimi/fir070.html" target="_blank"
-			>IMMORTAL HARDCORE!!!! VOL.5 -Voice Of Terror-</a
-		>
-		(Comp., 2019-05-08, Fujimi Industry Records)
-	</li>
-	<li>
-		<a href="https://miso016.tumblr.com/" target="_blank">miso-nicomi collection vol.2 THE EDM</a>
-		(Comp., 2019-04-28, miso-nicomi records)
-	</li>
-	<li>
-		<a
-			href="https://speedcoreworldwideaudionetlabel.bandcamp.com/album/circles-of-hell-vol-4"
-			target="_blank">Circles Of Hell VOL.4</a
-		>
-		(Comp., 2019-01-25, Speedcore Worldwide Audio Netlabel)
-	</li>
-</ul>
-<h3>2018</h3>
-<ul>
-	<li>
-		<a
-			href="https://darkstepimplantant.bandcamp.com/album/darkstep-implantant-compilation-vol-3"
-			target="_blank">Darkstep Implantant Compilation vol. 3</a
-		>
-		(Comp., 2018-12-25, Darkstep Implantant)
-	</li>
-	<li>
-		<a href="http://sound.jp/fujimi/fir060.html" target="_blank">Homicide Vol.1</a>
-		(Comp., 2018-12-14, Fujimi Industry Records)
-	</li>
-	<li>
-		<a href="http://006.lolicore.jp/" target="_blank">虚空戦士 - まんだらけ BEATS EP</a>
-		(Remix, 2018-08-01, lolicore.jp)
-	</li>
-	<li>
-		<a href="http://sound.jp/fujimi/fir045.html" target="_blank"
-			>IMMORTAL HARDCORE!!!! VOL.4 -Ghost In The Hell-</a
-		>
-		(Comp., 2018-04-29, Fujimi Industry Records)
-	</li>
-</ul>
-<h3>2017</h3>
-<ul>
-	<li>
-		<a
-			href="https://speedcoreworldwideaudionetlabel.bandcamp.com/album/speedcore-worldwide-sampler-vol-4"
-			target="_blank"
-			>SPEEDCORE WORLDWIDE SAMPLER VOL.4 ᵈᵒʷᶰᶠᵃˡˡ ᵒᶠ ᵗʰᵉ ᵐᵃᶤᶰˢᵗʳᵉᵃᵐ
-		</a>
-		(Comp., 2017-09-14, Speedcore Worldwide Audio Netlabel)
-	</li>
-	<li>
-		<a href="https://industrialofsound.wixsite.com/dmtrecords/albums" target="_blank"
-			>THE BIG FAMILY OF MURDERCORE (II-Murder)</a
-		>
-		(Comp., 2017-05-15, Danger Murder Terror)
-	</li>
-	<li>
-		<a href="http://sound.jp/fujimi/fir029.html" target="_blank"
-			>IMMORTAL HARDCORE!!!! VOL.3 -Samurai Destroyer-</a
-		>
-		(Comp., 2017-05-10, Fujimi Industry Records)
-	</li>
-</ul>
-<h3>2016</h3>
-<ul>
-	<li>
-		<a href="https://sna001.tumblr.com/" target="_blank"
-			>Sadistik Noize Addiction Vol.1 - Fearless &amp; Reckless-</a
-		>
-		(Comp. 2016-10-30, Sadistik Noize Asylum)
-	</li>
-	<li>
-		<a href="http://sound.jp/fujimi/fir019.html" target="_blank"
-			>IMMORTAL HARDCORE!!!! VOL.2 -EXTRAZONE-
-		</a>
-		(Comp., 2016-09-07, Fujimi Industry Records)
-	</li>
-</ul>
+			<li>
+				<a href="https://sound.jp/fujimi/fir110.html" target="_blank">Homicide Vol.3</a>
+				(Comp., 2021-08-05, Fujimi Industry Records)
+			</li>
+			<li>
+				<a href="https://okuyukashiki.bandcamp.com/album/3" target="_blank">ばり好いコンピ3</a>
+				(Comp., 2021-04-25, 架空インターネットラジオ「ばり好いと〜よ！」)
+			</li>
+		</ul>
+		<h3>2020</h3>
+		<ul>
+			<li>
+				<a href="https://zisatsu.bandcamp.com/album/her-picture" target="_blank"
+					>彼女の写真 -Her picture-</a
+				>
+				(Comp., 2020-08-10, $uiciderecord$)
+			</li>
+			<li>
+				<a href="http://sound.jp/fujimi/fir088.html" target="_blank">Homicide Vol.2</a>
+				(Comp., 2020-06-19, Fujimi Industry Records)
+			</li>
+			<li>
+				<a href="https://okuyukashiki.bandcamp.com/" target="_blank">ばり好いコンピ</a>
+				(Comp., 2020-03-01, 架空インターネットラジオ「ばり好いと〜よ！」)
+			</li>
+		</ul>
+		<h3>2019</h3>
+		<ul>
+			<li>
+				<a href="https://sna003.tumblr.com/" target="_blank"
+					>Sadistik Noize Addiction Vol.2 -Violence of Hell-</a
+				>
+				(Comp., 2019-10-27, Sadistik Noize Asylum)
+			</li>
+			<li>
+				<a href="http://sound.jp/fujimi/fir078.html" target="_blank">Coakira - Sickle</a>
+				(Remix, 2019-10-27, Fujimi Industry Records)
+			</li>
+			<li>
+				<a href="http://sound.jp/fujimi/fir070.html" target="_blank"
+					>IMMORTAL HARDCORE!!!! VOL.5 -Voice Of Terror-</a
+				>
+				(Comp., 2019-05-08, Fujimi Industry Records)
+			</li>
+			<li>
+				<a href="https://miso016.tumblr.com/" target="_blank"
+					>miso-nicomi collection vol.2 THE EDM</a
+				>
+				(Comp., 2019-04-28, miso-nicomi records)
+			</li>
+			<li>
+				<a
+					href="https://speedcoreworldwideaudionetlabel.bandcamp.com/album/circles-of-hell-vol-4"
+					target="_blank">Circles Of Hell VOL.4</a
+				>
+				(Comp., 2019-01-25, Speedcore Worldwide Audio Netlabel)
+			</li>
+		</ul>
+		<h3>2018</h3>
+		<ul>
+			<li>
+				<a
+					href="https://darkstepimplantant.bandcamp.com/album/darkstep-implantant-compilation-vol-3"
+					target="_blank">Darkstep Implantant Compilation vol. 3</a
+				>
+				(Comp., 2018-12-25, Darkstep Implantant)
+			</li>
+			<li>
+				<a href="http://sound.jp/fujimi/fir060.html" target="_blank">Homicide Vol.1</a>
+				(Comp., 2018-12-14, Fujimi Industry Records)
+			</li>
+			<li>
+				<a href="http://006.lolicore.jp/" target="_blank">虚空戦士 - まんだらけ BEATS EP</a>
+				(Remix, 2018-08-01, lolicore.jp)
+			</li>
+			<li>
+				<a href="http://sound.jp/fujimi/fir045.html" target="_blank"
+					>IMMORTAL HARDCORE!!!! VOL.4 -Ghost In The Hell-</a
+				>
+				(Comp., 2018-04-29, Fujimi Industry Records)
+			</li>
+		</ul>
+		<h3>2017</h3>
+		<ul>
+			<li>
+				<a
+					href="https://speedcoreworldwideaudionetlabel.bandcamp.com/album/speedcore-worldwide-sampler-vol-4"
+					target="_blank"
+					>SPEEDCORE WORLDWIDE SAMPLER VOL.4 ᵈᵒʷᶰᶠᵃˡˡ ᵒᶠ ᵗʰᵉ ᵐᵃᶤᶰˢᵗʳᵉᵃᵐ
+				</a>
+				(Comp., 2017-09-14, Speedcore Worldwide Audio Netlabel)
+			</li>
+			<li>
+				<a href="https://industrialofsound.wixsite.com/dmtrecords/albums" target="_blank"
+					>THE BIG FAMILY OF MURDERCORE (II-Murder)</a
+				>
+				(Comp., 2017-05-15, Danger Murder Terror)
+			</li>
+			<li>
+				<a href="http://sound.jp/fujimi/fir029.html" target="_blank"
+					>IMMORTAL HARDCORE!!!! VOL.3 -Samurai Destroyer-</a
+				>
+				(Comp., 2017-05-10, Fujimi Industry Records)
+			</li>
+		</ul>
+		<h3>2016</h3>
+		<ul>
+			<li>
+				<a href="https://sna001.tumblr.com/" target="_blank"
+					>Sadistik Noize Addiction Vol.1 - Fearless &amp; Reckless-</a
+				>
+				(Comp. 2016-10-30, Sadistik Noize Asylum)
+			</li>
+			<li>
+				<a href="http://sound.jp/fujimi/fir019.html" target="_blank"
+					>IMMORTAL HARDCORE!!!! VOL.2 -EXTRAZONE-
+				</a>
+				(Comp., 2016-09-07, Fujimi Industry Records)
+			</li>
+		</ul>
+	</article>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,66 @@
 <svelte:head>
-	<title>FATAL WOVND</title>
-	<meta name="description" content="An egocentric platform, run by TohLPeaks" />
+	<title>FATAL WOVND, run by TohLPeaks</title>
+	<meta name="description" content="An egocentric platform" />
 </svelte:head>
 
-<h1>FATAL WOVND</h1>
-<p>An egocentric platform, run by TohLPeaks</p>
-
-<h2>Current Status</h2>
-
-<h2>Portfolio</h2>
-<ul>
-	<li><a href="/discography">Discography</a></li>
-	<li><a href="/past-shows">Past Shows</a></li>
-</ul>
-
-<h2>Links</h2>
-<h3>Music / Video</h3>
-<ul>
-	<li>
-		<a href="https://tohlpeaks.bandcamp.com/" target="_blank" rel="me">bandcamp</a>
-	</li>
-	<li>
-		<a href="https://soundcloud.com/tohlpeaks" target="_blank" rel="me">SoundCloud</a>
-	</li>
-	<li>
-		<a
-			href="https://open.spotify.com/intl-ja/artist/2LeJ6ZHEK9gOMI4vMgYnSc?si=C4fwsRwRS4SgV1RBcBc80Q"
-			target="_blank"
-			rel="me">Spotify</a
-		>
-	</li>
-	<li>
-		<a href="https://www.youtube.com/@tohlpeaks" target="_blank" rel="me">YouTube</a>
-	</li>
-</ul>
-
-<address>
-	<h3>Social / Contact</h3>
-	<ul>
-		<li>
-			<a href="https://www.instagram.com/tohlpeaks/" target="_blank" rel="me">Instagram</a>
-		</li>
-		<li>
-			<a href="https://x.com/tohlpeaks" target="_blank" rel="me">X</a>
-		</li>
-		<li>
-			<a href="https://discord.gg/MnKBeR3eNm" target="_blank" rel="me">Discord</a>
-		</li>
-		<li>mail: tohlpeaks(at)proton(dot)me</li>
-	</ul>
-</address>
-
-<h2>Misc</h2>
-<ul>
-	<li>
-		<a href="https://log.tohlpeaks.party/" target="_blank" rel="me">Blog</a>
-	</li>
-	<li>
-		<a href="https://discord.gg/MnKBeR3eNm" target="_blank">Discord Server</a>
-	</li>
-</ul>
+<main>
+	<article>
+		<h1>FATAL WOVND</h1>
+		<p>An egocentric platform, run by TohLPeaks</p>
+		<h2>Announcement</h2>
+		<h3>Next show</h3>
+		<p>
+			system_crash - <time>2025-01-24 22:00</time> @ Bhanc, Osaka:
+			<a href="https://x.com/ruehaduki/status/1880249440126337279" target="_blank">Info</a>
+		</p>
+		<h2>Portfolio</h2>
+		<ul>
+			<li><a href="/discography">Discography</a></li>
+			<li><a href="/past-shows">Past shows</a></li>
+		</ul>
+		<h2>Links</h2>
+		<h3>Music / Video</h3>
+		<ul>
+			<li>
+				<a href="https://tohlpeaks.bandcamp.com/" target="_blank" rel="me">bandcamp</a>
+			</li>
+			<li>
+				<a href="https://soundcloud.com/tohlpeaks" target="_blank" rel="me">SoundCloud</a>
+			</li>
+			<li>
+				<a href="https://www.youtube.com/@tohlpeaks" target="_blank" rel="me">YouTube</a>
+			</li>
+			<li>
+				<a
+					href="https://ffm.bio/tohlpeaks"
+					target="_blank"
+					rel="me">Other digital platforms</a
+				>
+			</li>
+		</ul>
+		<address>
+			<h3>Social / Contact</h3>
+			<ul>
+				<li>
+					<a href="https://www.instagram.com/tohlpeaks/" target="_blank" rel="me">Instagram</a>
+				</li>
+				<li>
+					<a href="https://x.com/tohlpeaks" target="_blank" rel="me">X</a>
+				</li>
+				<li>
+					<a href="https://discord.gg/MnKBeR3eNm" target="_blank" rel="me">Discord</a>
+				</li>
+				<li>mail: tohlpeaks(at)proton(dot)me</li>
+			</ul>
+		</address>
+		<h2>Misc</h2>
+		<ul>
+			<li>
+				<a href="https://log.tohlpeaks.party/" target="_blank" rel="me">Blog</a>
+			</li>
+			<li>
+				<a href="https://discord.gg/MnKBeR3eNm" target="_blank">Discord server</a>
+			</li>
+		</ul>
+	</article>
+</main>
