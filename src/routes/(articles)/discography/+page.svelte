c770748a58813c829e5c4abce1c56d6e825a1def
peaksvndvalleys <39687819+peaksvndvalleys@users.noreply.github.com>
2025-01-23 10:36:30 +0900

    fix(discography page): update compilation labels from "Comp." to "V.A. Compilation" for consistency and clarity in the discography section

--- src/routes/(articles)/discography/+page.svelte
+++ src/routes/(articles)/discography/+page.svelte
@@ -74,7 +74,7 @@
 				<a href="https://sound.jp/fujimi/fir167.html" target="_blank"
 					>IMMORTAL HARDCORE!!!! VOL.7 -Bloody Deathmatch-</a
 				>
-				(Comp., 2024-04-28, Fujimi Industry Records)
+				(V.A. Compilation, 2024-04-28, Fujimi Industry Records)
 			</li>
 		</ul>
 		<h3>2023</h3>
@@ -83,13 +83,13 @@
 				<a href="https://zisatsu.bandcamp.com/album/uicide-elect-vol-2" target="_blank"
 					>$UICIDE​$​ELECT Vol​.​2</a
 				>
-				(Comp., 2023-08-04, $uiciderecord$)
+				(V.A. Compilation, 2023-08-04, $uiciderecord$)
 			</li>
 			<li>
 				<a href="https://zisatsu.bandcamp.com/album/uicide-elect-vol-1" target="_blank"
 					>$UICIDE​$​ELECT Vol​.​1</a
 				>
-				(Comp., 2023-01-27, $uiciderecord$)
+				(V.A. Compilation, 2023-01-27, $uiciderecord$)
 			</li>
 		</ul>
 		<h3>2022</h3>
@@ -98,19 +98,19 @@
 				<a href="https://lossokarecords.bandcamp.com/album/genocide-afterparty" target="_blank">
 					Genocide Afterparty</a
 				>
-				(Comp., 2022-06-16, Lossoka Records)
+				(V.A. Compilation, 2022-06-16, Lossoka Records)
 			</li>
 			<li>
 				<a href="https://sound.jp/fujimi/fir125.html" target="_blank"
 					>IMMORTAL HARDCORE!!!! VOL.6 -Black Out-</a
 				>
-				(Comp., 2022-04-24, Fujimi Industry Records)
+				(V.A. Compilation, 2022-04-24, Fujimi Industry Records)
 			</li>
 			<li>
 				<a href="https://zisatsu.bandcamp.com/album/depressive-breakcore-vol-4" target="_blank"
 					>Depressive Breakcore vol.4</a
 				>
-				(Comp., 2022-04-02, $uiciderecord$)
+				(V.A. Compilation, 2022-04-02, $uiciderecord$)
 			</li>
 		</ul>
 		<h3>2021</h3>
@@ -119,16 +119,16 @@
 				<a href="https://zisatsu.bandcamp.com/album/hers-movie" target="_blank"
 					>彼女の映画 ~Hers Movie~</a
 				>
-				(Comp., 2021-08-06, $uiciderecord$)
+				(V.A. Compilation, 2021-08-06, $uiciderecord$)
 			</li>
 
 			<li>
 				<a href="https://sound.jp/fujimi/fir110.html" target="_blank">Homicide Vol.3</a>
-				(Comp., 2021-08-05, Fujimi Industry Records)
+				(V.A. Compilation, 2021-08-05, Fujimi Industry Records)
 			</li>
 			<li>
 				<a href="https://okuyukashiki.bandcamp.com/album/3" target="_blank">ばり好いコンピ3</a>
-				(Comp., 2021-04-25, 架空インターネットラジオ「ばり好いと〜よ！」)
+				(V.A. Compilation, 2021-04-25, 架空インターネットラジオ「ばり好いと〜よ！」)
 			</li>
 		</ul>
 		<h3>2020</h3>
@@ -137,15 +137,15 @@
 				<a href="https://zisatsu.bandcamp.com/album/her-picture" target="_blank"
 					>彼女の写真 -Her picture-</a
 				>
-				(Comp., 2020-08-10, $uiciderecord$)
+				(V.A. Compilation, 2020-08-10, $uiciderecord$)
 			</li>
 			<li>
 				<a href="http://sound.jp/fujimi/fir088.html" target="_blank">Homicide Vol.2</a>
-				(Comp., 2020-06-19, Fujimi Industry Records)
+				(V.A. Compilation, 2020-06-19, Fujimi Industry Records)
 			</li>
 			<li>
 				<a href="https://okuyukashiki.bandcamp.com/" target="_blank">ばり好いコンピ</a>
-				(Comp., 2020-03-01, 架空インターネットラジオ「ばり好いと〜よ！」)
+				(Collaboration, 2020-03-01, 架空インターネットラジオ「ばり好いと〜よ！」)
 			</li>
 		</ul>
 		<h3>2019</h3>
@@ -154,7 +154,7 @@
 				<a href="https://sna003.tumblr.com/" target="_blank"
 					>Sadistik Noize Addiction Vol.2 -Violence of Hell-</a
 				>
-				(Comp., 2019-10-27, Sadistik Noize Asylum)
+				(V.A. Compilation, 2019-10-27, Sadistik Noize Asylum)
 			</li>
 			<li>
 				<a href="http://sound.jp/fujimi/fir078.html" target="_blank">Coakira - Sickle</a>
@@ -164,20 +164,20 @@
 				<a href="http://sound.jp/fujimi/fir070.html" target="_blank"
 					>IMMORTAL HARDCORE!!!! VOL.5 -Voice Of Terror-</a
 				>
-				(Comp., 2019-05-08, Fujimi Industry Records)
+				(V.A. Compilation, 2019-05-08, Fujimi Industry Records)
 			</li>
 			<li>
 				<a href="https://miso016.tumblr.com/" target="_blank"
 					>miso-nicomi collection vol.2 THE EDM</a
 				>
-				(Comp., 2019-04-28, miso-nicomi records)
+				(V.A. Compilation, 2019-04-28, miso-nicomi records)
 			</li>
 			<li>
 				<a
 					href="https://speedcoreworldwideaudionetlabel.bandcamp.com/album/circles-of-hell-vol-4"
 					target="_blank">Circles Of Hell VOL.4</a
 				>
-				(Comp., 2019-01-25, Speedcore Worldwide Audio Netlabel)
+				(V.A. Compilation, 2019-01-25, Speedcore Worldwide Audio Netlabel)
 			</li>
 		</ul>
 		<h3>2018</h3>
@@ -187,11 +187,11 @@
 					href="https://darkstepimplantant.bandcamp.com/album/darkstep-implantant-compilation-vol-3"
 					target="_blank">Darkstep Implantant Compilation vol. 3</a
 				>
-				(Comp., 2018-12-25, Darkstep Implantant)
+				(V.A. Compilation, 2018-12-25, Darkstep Implantant)
 			</li>
 			<li>
 				<a href="http://sound.jp/fujimi/fir060.html" target="_blank">Homicide Vol.1</a>
-				(Comp., 2018-12-14, Fujimi Industry Records)
+				(V.A. Compilation, 2018-12-14, Fujimi Industry Records)
 			</li>
 			<li>
 				<a href="http://006.lolicore.jp/" target="_blank">虚空戦士 - まんだらけ BEATS EP</a>
@@ -201,7 +201,7 @@
 				<a href="http://sound.jp/fujimi/fir045.html" target="_blank"
 					>IMMORTAL HARDCORE!!!! VOL.4 -Ghost In The Hell-</a
 				>
-				(Comp., 2018-04-29, Fujimi Industry Records)
+				(V.A. Compilation, 2018-04-29, Fujimi Industry Records)
 			</li>
 		</ul>
 		<h3>2017</h3>
@@ -212,19 +212,19 @@
 					target="_blank"
 					>SPEEDCORE WORLDWIDE SAMPLER VOL.4 ᵈᵒʷᶰᶠᵃˡˡ ᵒᶠ ᵗʰᵉ ᵐᵃᶤᶰˢᵗʳᵉᵃᵐ
 				</a>
-				(Comp., 2017-09-14, Speedcore Worldwide Audio Netlabel)
+				(V.A. Compilation, 2017-09-14, Speedcore Worldwide Audio Netlabel)
 			</li>
 			<li>
 				<a href="https://industrialofsound.wixsite.com/dmtrecords/albums" target="_blank"
 					>THE BIG FAMILY OF MURDERCORE (II-Murder)</a
 				>
-				(Comp., 2017-05-15, Danger Murder Terror)
+				(V.A. Compilation, 2017-05-15, Danger Murder Terror)
 			</li>
 			<li>
 				<a href="http://sound.jp/fujimi/fir029.html" target="_blank"
 					>IMMORTAL HARDCORE!!!! VOL.3 -Samurai Destroyer-</a
 				>
-				(Comp., 2017-05-10, Fujimi Industry Records)
+				(V.A. Compilation, 2017-05-10, Fujimi Industry Records)
 			</li>
 		</ul>
 		<h3>2016</h3>
@@ -233,13 +233,13 @@
 				<a href="https://sna001.tumblr.com/" target="_blank"
 					>Sadistik Noize Addiction Vol.1 - Fearless &amp; Reckless-</a
 				>
-				(Comp. 2016-10-30, Sadistik Noize Asylum)
+				(V.A. Compilation 2016-10-30, Sadistik Noize Asylum)
 			</li>
 			<li>
 				<a href="http://sound.jp/fujimi/fir019.html" target="_blank"
 					>IMMORTAL HARDCORE!!!! VOL.2 -EXTRAZONE-
 				</a>
-				(Comp., 2016-09-07, Fujimi Industry Records)
+				(V.A. Compilation, 2016-09-07, Fujimi Industry Records)
 			</li>
 		</ul>
 	</article>
