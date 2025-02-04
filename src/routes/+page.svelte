d58a9e38e45e8e2e1af9fc88e725c5a61f4c757f
peaksvndvalleys <39687819+peaksvndvalleys@users.noreply.github.com>
2025-02-04 16:44:05 +0900

    style: remove target="_blank" from links for consistency and improved accessibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,31 +45,29 @@
 			<h3>Music / Video</h3>
 			<ul>
 				<li>
-					<a href="https://tohlpeaks.bandcamp.com/" target="_blank" rel="me">bandcamp</a>
+					<a href="https://tohlpeaks.bandcamp.com/" rel="me">bandcamp</a>
 				</li>
 				<li>
-					<a href="https://soundcloud.com/tohlpeaks" target="_blank" rel="me">SoundCloud</a>
+					<a href="https://soundcloud.com/tohlpeaks" rel="me">SoundCloud</a>
 				</li>
 				<li>
-					<a href="https://www.youtube.com/@tohlpeaks" target="_blank" rel="me">YouTube</a>
+					<a href="https://www.youtube.com/@tohlpeaks" rel="me">YouTube</a>
 				</li>
 				<li>
-					<a href="https://ffm.bio/tohlpeaks" target="_blank" rel="me">Other digital platforms</a>
+					<a href="https://ffm.bio/tohlpeaks" rel="me">Other digital platforms</a>
 				</li>
 			</ul>
 			<address>
 				<h3>Social / Contact</h3>
 				<ul>
 					<li>
-						<a href="https://www.instagram.com/tohlpeaks/" target="_blank" rel="me">Instagram</a>
+						<a href="https://www.instagram.com/tohlpeaks/" rel="me">Instagram</a>
 					</li>
 					<li>
-						<a href="https://x.com/tohlpeaks" target="_blank" rel="me">X</a>
+						<a href="https://x.com/tohlpeaks" rel="me">X</a>
 					</li>
 					<li>
-						<a href="https://discordapp.com/users/283132253465477120" target="_blank" rel="me"
-							>Discord</a
-						>
+						<a href="https://discordapp.com/users/283132253465477120" rel="me">Discord</a>
 					</li>
 					<li>mail: tohlpeaks(at)proton(dot)me</li>
 				</ul>
@@ -77,15 +75,13 @@
 			<h2>Misc</h2>
 			<ul>
 				<li>
-					<a href="https://log.tohlpeaks.party/" target="_blank" rel="me"
-						>Blog (written in Japanese)</a
-					>
+					<a href="https://log.tohlpeaks.party/" rel="me">Blog (written in Japanese)</a>
 				</li>
 				<li>
-					<a href="https://discord.gg/MnKBeR3eNm" target="_blank">Discord server</a>
+					<a href="https://discord.gg/MnKBeR3eNm">Discord server</a>
 				</li>
 				<li>
-					<a href="https://www.paypal.com/paypalme/tohlpeaks" target="_blank">Donate via PayPal</a>
+					<a href="https://www.paypal.com/paypalme/tohlpeaks">Donate via PayPal</a>
 				</li>
 				<li>
 					<a href="/recommendations">Recommendations</a>
