4a82dcdd3d8d4cd15d7a29a2db2cf288885ad4e1
peaksvndvalleys <39687819+peaksvndvalleys@users.noreply.github.com>
2025-02-13 13:37:29 +0900

    feat(recommendations): add hreflang attributes to links for better SEO and accessibility feat(recommendations): include additional sections for blogs/media and record shops to enhance content richness

--- src/routes/(articles)/recommendations/+page.svelte
+++ src/routes/(articles)/recommendations/+page.svelte
@@ -19,7 +19,10 @@
 		<h2>Individuals</h2>
 		<ul>
 			<li>
-				<a href="https://www.cannorin.net/">cannorin</a>
+				<a href="https://www.cannorin.net/" hreflang="ja">cannorin</a>
+			</li>
+			<li>
+				<a href="https://deathinsurance.online/" hreflang="en-US">death insurance</a>
 			</li>
 			<li>
 				<a href="https://eulalie.fun/">Eulalie</a>
@@ -38,10 +41,10 @@
 		<h3>Venues</h3>
 		<ul>
 			<li>
-				<a href="https://nuthings.wordpress.com/">environment 0g</a>
+				<a href="https://nuthings.wordpress.com/" hreflang="ja">environment 0g</a>
 			</li>
 			<li>
-				<a href="https://namba-bears.main.jp">Namba BEARS</a>
+				<a href="https://namba-bears.main.jp" hreflang="ja">Namba BEARS</a>
 			</li>
 		</ul>
 		<h3>Internet radios</h3>
@@ -51,6 +54,24 @@
 			</li>
 			<li>
 				<a href="https://somnyan.stream/">Somnyan In-ter-face</a>
+			</li>
+		</ul>
+		<h3>Blogs / Media</h3>
+		<ul>
+			<li>
+				<a href="https://ghz.tokyo/" hreflang="ja">GHz</a>
+			</li>
+			<li>
+				<a href="https://indiegrab.jp/" hreflang="ja">indiegrab</a>
+			</li>
+		</ul>
+		<h3>Record shops</h3>
+		<ul>
+			<li>
+				<a href="https://enban.cart.fc2.com/" hreflang="ja">円盤</a>
+			</li>
+			<li>
+				<a href="https://naminohana-records.com/" hreflang="ja">naminohana records</a>
 			</li>
 		</ul>
 	</article>
