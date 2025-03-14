d1b77415d5f1e00de0c29665228f416b90714a62
peaksvndvalleys <39687819+peaksvndvalleys@users.noreply.github.com>
2025-03-14 21:02:49 +0900

    chore(page.svelte): update announcement section to reflect the end of the tour and remove outdated tour details chore(images): delete obsolete tour flyer image as it is no longer needed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,32 +8,8 @@
 		<h1>FATAL WOVND</h1>
 		<em>An egocentric platform, run by <strong>TohLPeaks</strong></em>
 		<h2>Announcement</h2>
-		<h3>nonsense superstar tour 2k25</h3>
-		<strong>I will tour in Spain and UK.</strong>
-		<ul>
-			<li>
-				<time datetime="2025-02-22">22/2/2025</time> - Sala Taro / Barcelona, Spain:
-				<a href="https://www.instagram.com/dayofthedroids/reel/DFEWpDACYyr/">Info on Instagram</a>
-			</li>
-			<li>
-				<time datetime="2025-02-27">27/2/2025</time> - Sala Siroco / Madrid, Spain:
-				<a
-					href="https://siroco.es/eventos/day-of-the-droids-ketapasando-y-voidsent-w-eulalie-live-tohlpeaks-live-autopsy-protocol-akani-live-miut/"
-					>Info</a
-				>
-			</li>
-			<li>
-				<time datetime="2025-03-07">7/3/2025</time> - The Red Lion / Bristol, UK:
-				<a
-					href="https://www.headfirstbristol.co.uk/whats-on/red-lion-bs5/fri-7-mar-big-chip-nrg-presents-tohlpeaks-eulalie-122154"
-					>Info</a
-				>
-			</li>
-		</ul>
-		<img
-			src="/images/index/nonsense-superstar-tour-2k25.jpg"
-			alt="tohlpeaks nonsense superstar tour 2k25 tour flyer"
-		/>
+		<h3>The tour is over</h3>
+		<p>Thank you very much everyone.</p>
 		<nav>
 			<h2>Works</h2>
 			<ul>
