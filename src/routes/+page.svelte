88c1ac920bb30a72c018e4696da6c8484e405555
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-04-22 17:24:52 +0900

    style(page.svelte): reorganize and clean up the structure of the navigation section for better readability and maintainability

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,64 +9,60 @@
 	<h2>Announcement</h2>
 	<h3>New singles out</h3>
 	<p>Check out <a href="/discography/2025-singles">this page</a></p>
-	<nav>
-		<h2>Works</h2>
-		<ul>
-			<li><a href="/discography">Discography</a></li>
-			<li><a href="/past-shows">Past shows</a></li>
-		</ul>
-		<h2>Links</h2>
-		<h3>Music / Video</h3>
-		<ul>
-			<li>
-				<a href="https://tohlpeaks.bandcamp.com/" rel="me" translate="no">bandcamp</a>
-			</li>
-			<li>
-				<a href="https://soundcloud.com/tohlpeaks" rel="me" translate="no">SoundCloud</a>
-			</li>
-			<li>
-				<a href="https://www.youtube.com/@tohlpeaks" rel="me" translate="no">YouTube</a>
-			</li>
-			<li>
-				<a href="https://ffm.bio/tohlpeaks" rel="me">Other digital platforms (feature.fm)</a>
-			</li>
-		</ul>
-		<address>
-			<h3>Social / Contact</h3>
-			<ul>
-				<li>
-					<a href="https://www.instagram.com/tohlpeaks/" rel="me" translate="no">Instagram</a>
-				</li>
-				<li>
-					<a href="https://bsky.app/profile/tohlpeaks.bsky.social" rel="me" translate="no"
-						>Bluesky</a
-					>
-				</li>
-				<li>
-					<a href="https://x.com/tohlpeaks" rel="me">X</a>
-				</li>
-				<li>
-					<a href="https://discordapp.com/users/283132253465477120" rel="me" translate="no"
-						>Discord (profile)</a
-					>
-				</li>
-				<li>mail: tohlpeaks(at)proton(dot)me</li>
-			</ul>
-		</address>
-		<h2>Misc</h2>
+	<h2>Works</h2>
+	<ul>
+		<li><a href="/discography">Discography</a></li>
+		<li><a href="/past-shows">Past shows</a></li>
+	</ul>
+	<h2>Links</h2>
+	<h3>Music / Video</h3>
+	<ul>
+		<li>
+			<a href="https://tohlpeaks.bandcamp.com/" rel="me" translate="no">bandcamp</a>
+		</li>
+		<li>
+			<a href="https://soundcloud.com/tohlpeaks" rel="me" translate="no">SoundCloud</a>
+		</li>
+		<li>
+			<a href="https://www.youtube.com/@tohlpeaks" rel="me" translate="no">YouTube</a>
+		</li>
+		<li>
+			<a href="https://ffm.bio/tohlpeaks" rel="me">Other digital platforms (feature.fm)</a>
+		</li>
+	</ul>
+	<address>
+		<h3>Social / Contact</h3>
 		<ul>
 			<li>
-				<a href="https://log.tohlpeaks.party/" rel="me">Blog (written in Japanese)</a>
+				<a href="https://www.instagram.com/tohlpeaks/" rel="me" translate="no">Instagram</a>
 			</li>
 			<li>
-				<a href="https://discord.gg/MnKBeR3eNm">Discord server</a>
+				<a href="https://bsky.app/profile/tohlpeaks.bsky.social" rel="me" translate="no">Bluesky</a>
 			</li>
 			<li>
-				<a href="https://www.paypal.com/paypalme/tohlpeaks">Donate via PayPal</a>
+				<a href="https://x.com/tohlpeaks" rel="me">X</a>
 			</li>
 			<li>
-				<a href="/recommendations">Recommendations</a>
+				<a href="https://discordapp.com/users/283132253465477120" rel="me" translate="no"
+					>Discord (profile)</a
+				>
 			</li>
+			<li>mail: tohlpeaks(at)proton(dot)me</li>
 		</ul>
-	</nav>
+	</address>
+	<h2>Misc</h2>
+	<ul>
+		<li>
+			<a href="https://log.tohlpeaks.party/" rel="me">Blog (written in Japanese)</a>
+		</li>
+		<li>
+			<a href="https://discord.gg/MnKBeR3eNm">Discord server</a>
+		</li>
+		<li>
+			<a href="https://www.paypal.com/paypalme/tohlpeaks">Donate via PayPal</a>
+		</li>
+		<li>
+			<a href="/recommendations">Recommendations</a>
+		</li>
+	</ul>
 </article>
