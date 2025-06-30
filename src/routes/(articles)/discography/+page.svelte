3c9774480b9f6849365449b721bcebd2cf112e63
ぴ <39687819+peaks-and-valleys@users.noreply.github.com>
2025-06-30 16:17:20 +0900

    Misc updates (#9)
    * chore: add vscode ignore rules and mcp configuration
    
    * fix(package.json): change sass to sass-embedded to resolve build issues
    
    * feat(types): declare module for svx files to allow importing
    
    feat(routes/(articles)/links/+page.svelte): add links page
    
    feat(routes/(articles)/links/Article.svx): create article component for links page
    
    * feat(+page.svelte): change to load Article component
    feat(Article.svx): create article component
    
    * refactor(pages): remove unused PageData type import
    
    * feat(svx.ts): declare module for svx files

---
+++ src/routes/(articles)/discography/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import HeadProperties from '$lib/components/HeadProperties.svelte';
+	import Article from './Article.svx';
+</script>
+
+<HeadProperties pageTitle="Discography" description="Discography list"></HeadProperties>
+
+<article>
+	<Article />
+</article>
---
+++ src/routes/(articles)/past-shows/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import HeadProperties from '$lib/components/HeadProperties.svelte';
+	import Article from './Article.svx';
+</script>
+
+<HeadProperties pageTitle="Past shows" description="Past shows list"></HeadProperties>
+
+<article>
+	<Article />
+</article>
