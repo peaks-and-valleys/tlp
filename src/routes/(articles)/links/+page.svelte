1a9cedabd58d9c194b10f99a5a225723b90c27d0
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-06-30 15:57:57 +0900

    feat(types): declare module for svx files to allow importing
    feat(routes/(articles)/links/+page.svelte): add links page
    
    feat(routes/(articles)/links/Article.svx): create article component for links page

---
+++ src/routes/(articles)/links/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import HeadProperties from '$lib/components/HeadProperties.svelte';
+	import Article from './Article.svx';
+</script>
+
+<HeadProperties pageTitle="Recommendations" description="Recommendation link list"></HeadProperties>
+
+<article>
+	<Article />
+</article>
