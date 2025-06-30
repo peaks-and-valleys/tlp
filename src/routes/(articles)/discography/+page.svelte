082131740577e4b9cd02ecd7d5d324a8cabffbad
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-06-30 16:16:07 +0900

    refactor(pages): remove unused PageData type import

--- src/routes/(articles)/discography/+page.svelte
+++ src/routes/(articles)/discography/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import HeadProperties from '$lib/components/HeadProperties.svelte';
 	import Article from './Article.svx';
-
-	let { data }: { data: PageData } = $props();
 </script>
 
 <HeadProperties pageTitle="Discography" description="Discography list"></HeadProperties>
--- src/routes/(articles)/past-shows/+page.svelte
+++ src/routes/(articles)/past-shows/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import HeadProperties from '$lib/components/HeadProperties.svelte';
 	import Article from './Article.svx';
-
-	let { data }: { data: PageData } = $props();
 </script>
 
 <HeadProperties pageTitle="Past shows" description="Past shows list"></HeadProperties>
