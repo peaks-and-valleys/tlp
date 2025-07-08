34aa39c2d5c8c8dabcbc635bf4caec42c6ef5463
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-07-08 16:29:26 +0900

    feat(about): add about page and links component to provide more information about the project

---
+++ src/routes/(general)/about/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import HeadProperties from '$lib/components/HeadProperties.svelte';
+	import Links from './Links.svelte';
+</script>
+
+<HeadProperties pageTitle="About" description="About TohLPeaks"></HeadProperties>
+
+<h1>About</h1>
+<p><strong>TohLPeaks</strong>: Genres unidentified. Creating confusion in Japan.</p>
+
+<Links />
