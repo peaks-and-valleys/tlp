e51c644edc750d7d528b1f889b6ef655fe231597
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-07-12 10:18:28 +0900

    feat(header.svelte): replace navigation with a custom Menu component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import Menu from './Menu.svelte';
 </script>
 
 <header>
@@ -8,31 +9,7 @@
 			<img src="/images/fatal-wovnd.svg" alt="FATAL WOVND" />
 		</a>
 	</div>
-	<nav aria-label="Main navigation">
-		<ul class="menu">
-			<li>
-				<a href="/about" aria-current={page.url.pathname === '/about' ? 'page' : undefined}>About</a
-				>
-			</li>
-			<li>
-				<a
-					href="/discography"
-					aria-current={page.url.pathname === '/discography' ? 'page' : undefined}>Discography</a
-				>
-			</li>
-			<li>
-				<a href="/shows" aria-current={page.url.pathname === '/shows' ? 'page' : undefined}>Shows</a
-				>
-			</li>
-			<li>
-				<a href="https://log.tohlpeaks.party/" rel="external me noopener noreferrer">Blog</a>
-			</li>
-			<li>
-				<a href="/links" aria-current={page.url.pathname === '/links' ? 'page' : undefined}>Links</a
-				>
-			</li>
-		</ul>
-	</nav>
+	<Menu />
 </header>
 
 <style lang="scss">
@@ -44,39 +21,6 @@
 				inline-size: 96px;
 				block-size: auto;
 				object-fit: contain;
-			}
-		}
-	}
-	.menu {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		list-style: none;
-		gap: var(--spacing-xs);
-		padding-inline-start: 0;
-		margin-block-end: 0;
-		justify-content: space-between;
-
-		a {
-			color: var(--bluegrey-20);
-			font-size: 0.875rem;
-			text-decoration: none;
-
-			@media (any-hover) {
-				&:hover {
-					background-color: var(--bluegrey-40);
-					color: var(--bluegrey-90);
-					@media (prefers-reduced-motion: no-preference) {
-						transition:
-							color 200ms ease-in-out,
-							background-color 200ms ease-in-out;
-					}
-				}
-			}
-
-			&[aria-current='page'] {
-				background-color: var(--bluegrey-40);
-				color: var(--bluegrey-90);
 			}
 		}
 	}
