11523e9fee8e2ab1cc61818abbc5e094a607d0fe
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-07-13 11:56:32 +0900

    fix(shows/fatal-club-case-03): add link to FATAL CLUB [CASE 03] page docs(shows/fatal-club-case-03): add images for all performers

---
+++ src/routes/(general)/shows/(info)/fatal-club-case-03/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import HeadProperties from '$lib/components/HeadProperties.svelte';
+	import Article from './Article.svx';
+
+	const metadata = {
+		pageTitle: 'FATAL CLUB [CASE 03]',
+		description: 'FATAL CLUB [CASE 03] at environment 0g, on 12th October 2024.',
+	};
+</script>
+
+<HeadProperties metadata={metadata} />
+
+<article lang="ja">
+	<Article />
+</article>
