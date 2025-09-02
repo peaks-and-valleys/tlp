07a9aa074ed7c5ca1159883ab093e869b541e9c9
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-09-02 23:05:04 +0900

    feat(routes/(general)/shows): add new route for FATAL CLUB [CASE 04] show with metadata and article content refactor(routes/(general)/shows/Article.svx): update existing Article component to include new show information fix(routes/(general)/shows/Article.svx): correct link to FATAL CLUB \[CASE 03\] show add(static/images/shows/fatal-club-case-04): add image for FATAL CLUB [CASE 04] show

---
+++ src/routes/(general)/shows/(info)/fatal-club-case-04/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import HeadProperties from '$lib/components/HeadProperties.svelte';
+	import Article from './Article.svx';
+
+	const metadata = {
+		pageTitle: 'FATAL CLUB [CASE 04]',
+		description: 'FATAL CLUB [CASE 04] at environment 0g, on 25th October 2025.'
+	};
+</script>
+
+<HeadProperties {metadata} />
+
+<article>
+	<Article />
+</article>
