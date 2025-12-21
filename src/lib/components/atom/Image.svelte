a7510c5805333bf2f284ac1441961def3d017355
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-12-21 16:05:47 +0900

    feat(Image.svelte): add marginBlockStartPx prop to configure margin-block-start and update CSS to use it

--- src/lib/components/atom/Image.svelte
+++ src/lib/components/atom/Image.svelte
@@ -2,6 +2,7 @@
 	import type { HTMLImgAttributes } from 'svelte/elements';
 
 	interface Props extends HTMLImgAttributes {
+		marginBlockStartPx?: string;
 		maxInlineSizePx?: string;
 		containIntrinsicSizePx?: string;
 	}
@@ -11,12 +12,14 @@
 		alt,
 		class: className,
 		style,
+		marginBlockStartPx,
 		maxInlineSizePx,
 		containIntrinsicSizePx,
 		...restProps
 	}: Props = $props();
 
 	const computedStyle = `
+		--margin-block-start-size: ${marginBlockStartPx ?? '16px'};
         --max-inline-size: ${maxInlineSizePx ?? '600px'};
         --intrinsic-size: ${containIntrinsicSizePx ?? '320px'}
         ${style || ''}
@@ -27,6 +30,7 @@
 
 <style>
 	img {
+		margin-block-start: var(--margin-block-start-size);
 		inline-size: min(100%, var(--max-inline-size));
 		content-visibility: auto;
 		contain-intrinsic-size: var(--intrinsic-size);
