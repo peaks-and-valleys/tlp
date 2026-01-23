b94b3c29a92a544447c26cc41e4550f10f04a4c5
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2026-01-23 20:24:10 +0900

    docs(discography): add article

---
+++ src/routes/(general)/discography/we-live-in-a-void/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import HeadProperties from '$lib/components/HeadProperties.svelte';
+	import Article from './Article.svx';
+
+	const metadata = {
+		pageTitle: 'we live in a void',
+		description: 'Released on 23rd January 2026'
+	};
+</script>
+
+<HeadProperties {metadata} />
+<article>
+	<Article />
+</article>
