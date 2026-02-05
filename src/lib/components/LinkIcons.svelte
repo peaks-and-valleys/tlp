16a60ef890947aecb70e5a15c26aea6c568b76a4
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2026-02-05 23:05:14 +0900

    chore(LinkIcons.svelte): update link destinations and icons, adjust rel attributes, and spacing gap

--- src/lib/components/LinkIcons.svelte
+++ src/lib/components/LinkIcons.svelte
@@ -1,12 +1,11 @@
 <!-- SVG icons are sourced from Tabler Icons -->
 <div class="wrapper">
-	<ul class="content" aria-label="External links to Tohlpeaks' music and social media">
+	<ul
+		class="content"
+		aria-label="External links to content distrubution platforms, shops, and social media"
+	>
 		<li>
-			<a
-				href="https://tohlpeaks.bandcamp.com/"
-				rel="external noopener noreferrer"
-				aria-label="Bandcamp"
-			>
+			<a href="https://tohlpeaks.bandcamp.com/" rel="external  noreferrer" aria-label="Bandcamp">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -18,6 +17,7 @@
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					class="icon"
+					aria-hidden="true"
 					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
 						d="M8.5 6h13.5l-7 12h-13z"
 					/></svg
@@ -26,9 +26,9 @@
 		</li>
 		<li>
 			<a
-				href="https://soundcloud.com/tohlpeaks"
-				rel="external noopener noreferrer"
-				aria-label="SoundCloud"
+				href="https://shop.tohlpeaks.party/"
+				rel="external  noreferrer"
+				aria-label="Merchandise Shop"
 			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -41,6 +41,27 @@
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					class="icon"
+					aria-hidden="true"
+					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+						d="M6.331 8h11.339a2 2 0 0 1 1.977 2.304l-1.255 8.152a3 3 0 0 1 -2.966 2.544h-6.852a3 3 0 0 1 -2.965 -2.544l-1.255 -8.152a2 2 0 0 1 1.977 -2.304"
+					/><path d="M9 11v-5a3 3 0 0 1 6 0v5" /></svg
+				>
+			</a>
+		</li>
+		<li>
+			<a href="https://soundcloud.com/tohlpeaks" rel="external  noreferrer" aria-label="SoundCloud">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="icon"
+					aria-hidden="true"
 					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
 						d="M17 11h1c1.38 0 3 1.274 3 3c0 1.657 -1.5 3 -3 3l-6 0v-10c3 0 4.5 1.5 5 4z"
 					/><path d="M9 8l0 9" /><path d="M6 17l0 -7" /><path d="M3 16l0 -2" /></svg
@@ -48,11 +69,7 @@
 			</a>
 		</li>
 		<li>
-			<a
-				href="https://www.youtube.com/@tohlpeaks"
-				rel="external noopener noreferrer"
-				aria-label="YouTube"
-			>
+			<a href="https://www.youtube.com/@tohlpeaks" rel="external  noreferrer" aria-label="YouTube">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -64,6 +81,7 @@
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					class="icon"
+					aria-hidden="true"
 					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
 						d="M2 8a4 4 0 0 1 4 -4h12a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-12a4 4 0 0 1 -4 -4v-8z"
 					/><path d="M10 9l5 3l-5 3z" /></svg
@@ -72,56 +90,8 @@
 		</li>
 		<li>
 			<a
-				href="https://open.spotify.com/artist/2LeJ6ZHEK9gOMI4vMgYnSc"
-				rel="external noopener noreferrer"
-				aria-label="Spotify"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="icon"
-					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-						d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"
-					/><path d="M8 11.973c2.5 -1.473 5.5 -.973 7.5 .527" /><path
-						d="M9 15c1.5 -1 4 -1 5 .5"
-					/><path d="M7 9c2 -1 6 -2 10 .5" /></svg
-				>
-			</a>
-		</li>
-		<li>
-			<a
-				href="https://music.apple.com/gb/artist/tohlpeaks/850080183"
-				rel="external noopener noreferrer"
-				aria-label="Apple Music"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="icon"
-					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-						d="M8.286 7.008c-3.216 0 -4.286 3.23 -4.286 5.92c0 3.229 2.143 8.072 4.286 8.072c1.165 -.05 1.799 -.538 3.214 -.538c1.406 0 1.607 .538 3.214 .538s4.286 -3.229 4.286 -5.381c-.03 -.011 -2.649 -.434 -2.679 -3.23c-.02 -2.335 2.589 -3.179 2.679 -3.228c-1.096 -1.606 -3.162 -2.113 -3.75 -2.153c-1.535 -.12 -3.032 1.077 -3.75 1.077c-.729 0 -2.036 -1.077 -3.214 -1.077z"
-					/><path d="M12 4a2 2 0 0 0 2 -2a2 2 0 0 0 -2 2" /></svg
-				>
-			</a>
-		</li>
-		<li>
-			<a
 				href="https://bsky.app/profile/tohlpeaks.party"
-				rel="external noopener noreferrer"
+				rel="external  noreferrer"
 				aria-label="Bluesky"
 			>
 				<svg
@@ -134,7 +104,8 @@
 					stroke-width="2"
 					stroke-linecap="round"
 					stroke-linejoin="round"
-					class="icon icon-tabler icons-tabler-outline icon-tabler-brand-bluesky"
+					aria-hidden="true"
+					class="icon"
 					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
 						d="M6.335 5.144c-1.654 -1.199 -4.335 -2.127 -4.335 .826c0 .59 .35 4.953 .556 5.661c.713 2.463 3.13 2.75 5.444 2.369c-4.045 .665 -4.889 3.208 -2.667 5.41c1.03 1.018 1.913 1.59 2.667 1.59c2 0 3.134 -2.769 3.5 -3.5c.333 -.667 .5 -1.167 .5 -1.5c0 .333 .167 .833 .5 1.5c.366 .731 1.5 3.5 3.5 3.5c.754 0 1.637 -.571 2.667 -1.59c2.222 -2.203 1.378 -4.746 -2.667 -5.41c2.314 .38 4.73 .094 5.444 -2.369c.206 -.708 .556 -5.072 .556 -5.661c0 -2.953 -2.68 -2.025 -4.335 -.826c-2.293 1.662 -4.76 5.048 -5.665 6.856c-.905 -1.808 -3.372 -5.194 -5.665 -6.856z"
 					/></svg
@@ -142,7 +113,7 @@
 			</a>
 		</li>
 		<li>
-			<a href="https://x.com/tohlpeaks" rel="external noopener noreferrer" aria-label="X (Twitter)">
+			<a href="https://x.com/tohlpeaks" rel="external  noreferrer" aria-label="X (Twitter)">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -153,7 +124,8 @@
 					stroke-width="2"
 					stroke-linecap="round"
 					stroke-linejoin="round"
-					class="icon icon-tabler icons-tabler-outline icon-tabler-brand-x"
+					class="icon"
+					aria-hidden="true"
 					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
 						d="M4 4l11.733 16h4.267l-11.733 -16l-4.267 0"
 					/><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" /></svg
@@ -164,7 +136,7 @@
 		<li>
 			<a
 				href="https://www.instagram.com/tohlpeaks"
-				rel="external noopener noreferrer"
+				rel="external  noreferrer"
 				aria-label="Instagram"
 			>
 				<svg
@@ -178,6 +150,7 @@
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					class="icon"
+					aria-hidden="true"
 					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
 						d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"
 					/><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M16.5 7.5v.01" /></svg
@@ -197,7 +170,7 @@
 		display: flex;
 		flex-direction: row;
 		flex-wrap: wrap;
-		gap: var(--spacing-xs);
+		gap: var(--spacing-s);
 		justify-content: space-around;
 		list-style: none;
 		padding: 0;
@@ -207,16 +180,16 @@
 		a {
 			text-decoration: none;
 			display: inline-block;
-			margin: -4px;
-			padding: 4px;
+			margin: -0.25rem;
+			padding: 0.25rem;
 
 			&[rel~='external']::after {
 				content: '';
 			}
 
 			.icon {
-				width: 100%;
-				height: 100%;
+				inline-size: 100%;
+				block-size: 100%;
 				stroke-width: 1.5;
 			}
 		}
