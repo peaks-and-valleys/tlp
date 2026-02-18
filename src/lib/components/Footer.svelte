e8516d43bedfcfd11acca54905ae668f9592fb25
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2026-02-18 23:18:13 +0900

    feat(footer): add ThemeToggle component and restructure footer layout for better organization style(link-icons): adjust justify-content to flex-end and add max-inline-size for improved alignment

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import Breadcrumbs from './Breadcrumbs.svelte';
 	import LinkIcons from './LinkIcons.svelte';
+	import ThemeToggle from './ThemeToggle.svelte';
 </script>
 
 <footer>
 	<Breadcrumbs />
 	<div class="content">
+		<div class="submenu">
+					<ThemeToggle />
+
 		<LinkIcons />
+
+		</div>
 		<p>
 			All contents on this website are licensed under <a
 				href="https://creativecommons.org/licenses/by-sa/4.0/"
@@ -22,15 +28,22 @@
 		margin-block-start: var(--spacing-m);
 		view-transition-name: footer;
 	}
+
 	.content {
 		padding: var(--spacing-s);
 		margin-block-start: var(--spacing-s);
-		border-block: 1px solid var(--darkgrey-3);
+		border-block: 1px solid var(--c-tertiary);
 
 		p {
 			margin-block-start: calc(var(--spacing-s) - var(--half-leading));
 			font-size: 0.75rem;
 			color: var(--c-secondary);
 		}
+	}
+
+	.submenu {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 	}
 </style>
