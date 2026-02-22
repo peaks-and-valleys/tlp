cc0f1ef3476a24a40be37a5075d78b8a7a654ea5
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2026-02-22 11:38:25 +0900

    fix(routes/+layout.svelte): remove view transition handling to avoid compatibility issues with browsers that do not support the View Transitions API

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,22 +2,10 @@
 	import { onMount, type Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
 	import './styles/app.scss';
-	import { onNavigate } from '$app/navigation';
 	import { pwaInfo } from 'virtual:pwa-info';
 	import { theme } from '$lib/utils/theme';
 
 	let { children }: { data: LayoutData; children: Snippet } = $props();
-
-	onNavigate((navigation) => {
-		if (!document.startViewTransition) return;
-
-		return new Promise((resolve) => {
-			document.startViewTransition(async () => {
-				resolve();
-				await navigation.complete;
-			});
-		});
-	});
 
 	onMount(() => {
 		// マウント時に現在のテーマを DOM に適用
