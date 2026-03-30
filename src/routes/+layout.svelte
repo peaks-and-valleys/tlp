0ba0e377a721135015a140e5cc49fb510175babb
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2026-03-30 22:36:28 +0900

    build: remove @vite-pwa/sveltekit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import { onMount, type Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
 	import './styles/app.scss';
-	import { pwaInfo } from 'virtual:pwa-info';
 	import { theme } from '$lib/utils/theme';
 
 	let { children }: { data: LayoutData; children: Snippet } = $props();
@@ -11,12 +10,6 @@
 		// マウント時に現在のテーマを DOM に適用
 		document.documentElement.setAttribute('data-theme', $theme);
 	});
-
-	let webManifest = $derived(pwaInfo ? pwaInfo.webManifest.linkTag : '');
 </script>
-
-<svelte:head>
-	{@html webManifest}
-</svelte:head>
 
 {@render children()}
